0e6674216ab9c3d9f16c105afd22156ad95b3103
hussa <hussainbalikai@gmail.com>
2026-03-04 12:10:18 +0530

    feat: Changes color of `SpiningLoader` React component to ' --theme-muted'

--- ai-chat-frontend/src/components/ui/SpiningLoader.jsx
+++ ai-chat-frontend/src/components/ui/SpiningLoader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SpiningLoader = () => {
     return (
         <div className="flex justify-center items-center">
-            <span className="w-6 h-6 inline-block border-[3px] border-dotted border-white rounded-full animate-[spin_2s_linear_infinite]" />
+            <span className="w-6 h-6 inline-block border-[3px] border-dotted border-theme-muted rounded-full animate-[spin_2s_linear_infinite]" />
         </div>
     )
 }
